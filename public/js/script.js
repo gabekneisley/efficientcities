09543ff9ed4a724d9dd88c27af871a679e6a5cac
Gabe Kneisley <gabekneisley@gmail.com>
2013-07-11 18:03:52 -0400

    fixing .js bug

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,1 @@
-$(document).ready(function() {
-	var converter = Markdown.getSanitizingConverter()
-
-  var editor1 = new Markdown.Editor(converter);
-  
-  editor1.run();
-});
+$(document).ready(function() {});
